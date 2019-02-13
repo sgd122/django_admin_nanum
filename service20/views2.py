08e413e1e03749420fd91a7a8e2255eb8f1d452e
sgd <dirrksdl@naver.com>
2019-02-13 13:56:32 +0900

    Create views2.py

---
+++ service20/views2.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from rest_framework import generics, serializers
+from django.http import HttpResponse,Http404, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_protect,csrf_exempt
+from django.shortcuts import get_object_or_404,render
+from rest_framework.response import Response
+from django.http import JsonResponse, HttpResponse,Http404, HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse
+from service10.models import *
+from service20.models import *
+from polls.models import Choice, Question
+from django.db.models import Max
+
+
+from collections  import OrderedDict
+import json
+# api/moim 으로 get하면 이 listview로 연결
+
+@csrf_exempt
+def msFn1(request):
+    ms_id = request.GET.get('ms_id_id', None)
+    #l_key1 = ms_apl.objects.filter(ms_id_id=ms_id).values_list('apl_id', flat=True) 
+
+    #print(l_key1);
+    #queryset = self.get_queryset()
+    #queryset = vm_nanum_stdt.objects.filter(apl_id__in=ms_id)
+    queryset = ms_apl.objects.filter(ms_id_id=ms_id)
+
+    queryset2 = cm_cnv_scr.objects.filter(eval_item='1')
+
+    for val in queryset:
+        print("1234")
+
+        print(ms_id)
+        print(val.apl_id)
+        print(val.score1)
+        print("0000")
+        print(val.score2)  
+
+        print("1111")
+        vl_cscore1 = (val.score1 / 100) * 100
+        print("2222")
+        print(vl_cscore1)
+        print("3333")
+        queryset2 = queryset2.filter(eval_item=1,max_scr__lt=vl_cscore1)
+        
+        queryset2 = queryset2.filter(eval_item=1,max_scr__lt ='15')
+        for val2 in queryset2:
+            print(val2.max_scr)
+
+        print(queryset2);
+        queryset2 = queryset2.filter(max_scr__lt=vl_cscore1)
+        print(queryset2);
+        print("4444")
+        print(queryset2[0].fin_scr)
+        print("5555")
+        
+
+
+
+        """
+        model_instance = ms_apl(
+            ms_id_id=ms_id_id, 
+            apl_no=apl_no, 
+            apl_id=apl_id,
+            score1=apl_id,
+            score2=apl_id,
+            score3=apl_id,
+            score4=apl_id,
+            score5=apl_id,
+            )
+        model_instance.save()
+        """
+
+
+    print(queryset[0].apl_id)
+    print(queryset)
+
+    message = "Ok"
+     
+    context = {'message': message,}
+    
+
+    #return HttpResponse(json.dumps(context), content_type="application/json")
+    return JsonResponse(context,json_dumps_params={'ensure_ascii': True})
