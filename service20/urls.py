9207924ccae2cf00560ca088452af2ab15b37e25
sgd <dirrksdl@naver.com>
2019-02-17 10:31:02 +0900

    변경

--- service20/urls.py
+++ service20/urls.py
@@ -44,8 +44,6 @@ urlpatterns = [
     ########################################################################################
     # 멘토링 프로그램 리스트 조회
     path('mpmgListPersionView/', mpmgListPersionView.as_view(), name='mpmgListPersionView'),
-    #멘토링 질문리스트
-    path('post_mt_quest/', post_mt_quest, name='post_mt_Quest'),
     # 멘토링 프로그램 질문유형 가져오기
     path('authUserInfoPersionQuest/', post_user_info_persion_Quest.as_view(), name='post_user_info_persion_Quest'), 
     # 멘토링 프로그램 신청
@@ -87,5 +85,6 @@ urlpatterns = [
     #멘토스쿨 환산표
     path('msFn1/', msFn1, name='msFn1'),
 
-    
+    #멘토링 질문리스트 (미사용)
+    path('post_mt_quest/', post_mt_quest, name='post_mt_Quest'),   
 ]
