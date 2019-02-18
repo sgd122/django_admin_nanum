ed7ee734dc74f685063aab7bea30ee49cec075ec
sgd <dirrksdl@naver.com>
2019-02-18 16:29:28 +0900

    수정

--- service20/views.py
+++ service20/views.py
@@ -304,6 +304,7 @@ class post_user_info_persion_view_Quest(generics.ListAPIView):
         query = "select B.std_detl_code_nm,B.rmrk,A.* from service20_mp_ans A, service20_com_cdd B where A.ques_no = B.std_detl_code and B.std_grp_code in (select att_cdh from service20_mp_sub where mp_id = '"+str(key1)+"') and A.mp_id = '"+str(key1)+"' and apl_id = '"+str(l_user_id)+"'"
         queryset = mp_ans.objects.raw(query)
 
+        print(query)
         
 
         serializer_class = self.get_serializer_class()
