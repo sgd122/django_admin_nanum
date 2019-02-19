1c40ea0355c7d483b4d8c4d023f3720159d0953b
sgd <dirrksdl@naver.com>
2019-02-19 09:40:26 +0900

    Update urls.py

--- service20/urls.py
+++ service20/urls.py
@@ -72,7 +72,7 @@ urlpatterns = [
     # 프로그램 수행계획서 상세
     path('MP0103M/detail/', MP0103M_Detail.as_view(), name='MP0103M_Detail'),
     # 프로그램 수행계획서 Insert
-    path('MP0103M/insert/', MP0103M_Insert.as_view(), name='MP0103M_Insert'),
+    path('MP0103M/insert/', MP0103M_Insert, name='MP0103M_Insert'),
     # 프로그램 수행계획서 Update
     # path('MP0103M/update/', MP0103M_Update.as_view(), name='MP0103M_Update'),
     ########################################################################################
