ffc6b804a08f458161130cba3dc98c70f0f7f424
sgd <dirrksdl@naver.com>
2019-02-22 15:24:33 +0900

    Update views.py

--- service20/views.py
+++ service20/views.py
@@ -986,8 +986,8 @@ class MP0101M_list(generics.ListAPIView):
 
         query += " ifnull((select 'Y' from service20_mp_mtr where yr = '"+str(l_yr)+"' and apl_id = '"+str(ida)+"' and mp_id = A.mp_id),'N') AS applyFlag,A.* from service20_mpgm A where A.yr='"+str(l_yr)+"' and A.apl_term='"+str(l_apl_term)+"' and (select count(1) from service20_mentor where apl_id = '"+ida+"') > 0 "
 
-        # query += " and if(A.status = '10' and now() > A.apl_to_dt, 'xx', A.status) "
-        # query += "  like ifnull(NULLIF('"+str(l_status)+"',''),'%%') || '%%' "
+        query += " and if(A.status = '10' and now() > A.apl_to_dt, 'xx', A.status) "
+        query += "  like ifnull(NULLIF('"+str(l_status)+"',''),'%%') || '%%' "
 
         query += " order by A.apl_fr_dt desc,A.apl_to_dt desc "
 
