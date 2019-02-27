29eab186d9bd7ed5004539c4f0e9537c1f2d5611
sgd <dirrksdl@naver.com>
2019-02-27 12:45:41 +0900

    Update views.py

--- service20/views.py
+++ service20/views.py
@@ -4589,3 +4589,10 @@ def com_upload(request):
         return HttpResponse('File Uploaded')
 
     return HttpResponse('Failed to Upload File')
+
+
+def namedtuplefetchall(cursor):
+    "Return all rows from a cursor as a namedtuple"
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
