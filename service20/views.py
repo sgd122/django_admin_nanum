e62bd497d2e5a6f52a1de0326c6427cf477275a1
Seong Gi Dong <dirrksdl@naver.com>
2019-03-14 09:15:17 +0900

    Update views.py

--- service20/views.py
+++ service20/views.py
@@ -4385,7 +4385,7 @@ class MP0101M_adm_list(generics.ListAPIView):
         query += " and use_indc = 'y'  "
         query += " and std_detl_code = C.status)) as status_nm,  "
 
-        query += " C.mp_name,B.pr_yr,B.pr_sch_yr,B.pr_term_div,A.* from service20_mp_mtr A left join service20_vw_nanum_stdt B on (A.apl_id = B.apl_id),service20_mpgm C where A.apl_id=B.apl_id and A.mp_id = C.mp_id and A.mp_id = '"+mp_ida+"' and A.apl_id='"+ida+"'"
+        query += " C.mp_name,B.pr_yr,B.pr_sch_yr,B.pr_term_div,A.* from service20_mp_mtr A left join service20_vw_nanum_stdt B on (A.apl_id = B.apl_id),service20_mpgm C where A.mp_id = C.mp_id and A.mp_id = '"+mp_ida+"' and A.apl_id='"+ida+"'"
         queryset = mp_mtr.objects.raw(query)
         print(query)
         serializer_class = self.get_serializer_class()
@@ -4939,12 +4939,11 @@ class MP0101M_report_list(generics.ListAPIView):
         query += "       cast( ((b.pr_sch_yr-1)*2)+(substr(b.pr_term_div,1,1)*1) as UNSIGNED) pr_term_cnt, "
         query += "       d.std_detl_code_nm AS mnt_term_nm,  "
         query += "       a.*  "
-        query += "FROM   service20_mp_mtr a,  " 
-        query += "       service20_vw_nanum_stdt b, "
+        query += "FROM   service20_mp_mtr a  " 
+        query += "    left join   service20_vw_nanum_stdt b(a.apl_id = b.apl_id), "
         query += "       service20_mpgm c,  "
         query += "       service20_com_cdd d "
         query += " WHERE a.mp_id = c.mp_id  "
-        query += "   AND a.apl_id = b.apl_id "
         query += "   AND a.mp_id = '"+str(mp_ida)+"'  "
         query += "   AND a.apl_id = '"+str(ida)+"' "
         query += "   AND d.std_grp_code  = 'MS0022' "
